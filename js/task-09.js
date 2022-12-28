b480395905fa32be069709d43888ccc5233f8c97
Evgeniy-Nazarov <perfectyam@gmail.com>
2022-12-27 23:14:37 -0500

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+const button = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+if (button) {
+  button.addEventListener('click', () => {
+    const randomColor = getRandomHexColor();
+    body.style.backgroundColor = randomColor;
+    color.textContent = randomColor;
+  });
+}
